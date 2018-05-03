bcaa232610a9e5eca8b3355b44c21145ef1b0e02
dlinz7 <linzdiegoa@gmail.com>
2018-05-02 21:57:52 -0500

    Standard tasks complete

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {people} from './people.js';
-import { PeopleList } from './PeopleList';
+import PeopleList from './PeopleList';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      People: [],
+      people: [],
     };
 
 
@@ -29,6 +29,7 @@ componentDidMount(){
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Friends</h1>
         </header>
+        <PeopleList people={this.state.people} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -2,9 +2,30 @@ import React from "react";
 
 const PeopleList = props => {
     
+    return( <div> 
+
+                {props.people.map(friend => {
+                  return (
+                      <div key={friend.first_name + friend.last_name + friend.email}>
+                            {friend.first_name} {friend.last_name} {friend.email}
+                      </div>
+                  );
+
+                })}
+
+           </div>
+
+            );
 
 
 
 
 
-}
+
+};
+    
+
+
+export default PeopleList;
+
+
